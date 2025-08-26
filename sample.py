b856588290f7a78348bcd542e819548e18d0ee60
Pathri Akhila <pathriakhila16@gmail.com>
2025-08-26 14:49:42 +0530

    sample.py

---
+++ sample.py
@@ -0,0 +1,113 @@
+# Write a program to get the following output. 
+
+print("Hi there")
+print("i am full stack Full stack developer")
+
+#Define Python variable, with your won Example?
+# defining variables
+student_name = "Sunitha"
+age = 20
+id = 101
+Location = "Hyderabad"
+Designation = "Employee"
+
+print("My name is ",student_name)
+print("My age is ",age)
+print("Id no ",id)
+print("I am from ",Location)
+print("I am a",Designation)
+
+#rite a Python program to print 'Hello' on screen and then print your name 
+#on a separate line
+print("Hello")
+print("Rajashekar")
+
+# print different world
+print("Hello")
+print("World")
+
+# Write a program to get the following output, by using the \t.
+# program to use input on this \t
+print("Raja\tshekar")
+print("pavan\tSurya")
+
+# Write a program for creating a variable with the integer numbers and call 
+#those variables in the print method.
+a = 10
+b = 20
+c = 30
+print("My first number is",a)
+print("My second numbrt is",b)
+print("My third number is",c)
+
+#rite a program for creating a variable as given below:
+java = 90
+python = 50
+Dotnet = 80
+Database = 70
+webdevelopment = 60
+print("My java marks is:",java)
+print("My python marks is :",python)
+print("My .net marks is:",Dotnet)
+print("My database marks is :",Database)
+print("My web development marks is:",webdevelopment)
+
+#. Write a program to print the given output?
+a,b,c = 10,20,30
+print("a:",a)
+print("b:",b)
+print("c:",c)
+
+# printing the symbols
+print("+++++++")
+print("--------")
+print("@@@@@@")
+print("#######")
+
+# datatypes with that in programs
+firstname = 'Raja'
+lastname = 'shekhar'
+phonenumber = '8977679239'
+Email = 'raja@peramsons.com'
+javamarks = 70
+pythonmarks = 90
+Database = 90
+webdevelopment = 80
+Totalmarks = 340
+Average = 85.00
+
+print("My first name ",firstname)
+print(type(firstname))
+print("My last name ",lastname)
+print(type(lastname))
+print("My phone number ",phonenumber)
+print(type(phonenumber))
+print("My email ",Email)
+print(type(Email))
+print("My Java Marks:",javamarks)
+print(type(javamarks))
+print("My python marks:",pythonmarks)
+print(type(pythonmarks))
+print("My Database marks:",Database)
+print(type(Database))
+print("My webdevelopment marks:",webdevelopment)
+print(type(webdevelopment))
+
+#Q13 : In Python, a variable must be declared before it is assigned a value:
+x = 10      # No declaration needed
+name = "Sam"
+pi = 3.14
+
+#Q14 :
+j = 37
+k = 52
+sum1 = j + k
+print("Sum:", sum1)
+
+#Re assign value 
+j = 8
+k = 3
+sum2 = j + k
+product = j * k
+print("Sum:", sum2)
+print("Product:", product) 
